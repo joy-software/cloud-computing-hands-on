6a0d724e8bcf841d43c24d9e7732742ca0e2b30b
otrack <0track@gmail.com>
2016-12-12 12:44:35 +0100

    spark:solution

--- spark/utils.py
+++ spark/utils.py
@@ -2,16 +2,19 @@ from operator import add
 
 # base operators
 
-def clean(rdd):
+def isClean(x):
     try: 
-        return rdd\
-          .map(lambda x: x.encode("ascii")\
-          .split(",")[6:10])\
-          .filter(lambda x : len(x)==4)\
-          .filter(lambda x : float(x[0])<>0 and float(x[1])<>0 and float(x[2])<>0 and float(x[3])<>0)\
-          .map(lambda x: (x[0]+":"+x[1]+":"+x[2]+":"+x[3],1))
+        return float(x[0])<>0 and float(x[1])<>0 and float(x[2])<>0 and float(x[3])<>0
     except ValueError:
         return None
+
+def clean(rdd):
+    return rdd\
+      .map(lambda x: x.encode("ascii")\
+      .split(",")[6:10])\
+      .filter(lambda x : len(x)==4)\
+      .filter(isClean)\
+      .map(lambda x: (x[0]+":"+x[1]+":"+x[2]+":"+x[3],1))
                    
 def topK(rdd,k):
     return rdd.reduceByKey(add).top(k,key=lambda item: item[1] )
